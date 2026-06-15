846497be39f7d360c6aed978aa3054bf80a78983
jimmykuek <jimmykuek94@gmail.com>
2026-06-15 18:38:14 +0800

    update auto carry to agreement and checklist

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v10';
+const CACHE = 'gp-hub-v11';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
