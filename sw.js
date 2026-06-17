302e39dab321147a763de76cce202daaaf270e59
jimmykuek <jimmykuek94@gmail.com>
2026-06-17 10:00:57 +0800

    gp minigame update

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v24';
+const CACHE = 'gp-hub-v25';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
