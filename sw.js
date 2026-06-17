aec4138a913b6fd97fbbc4dc2d3c8c5d34602880
jimmykuek <jimmykuek94@gmail.com>
2026-06-17 09:38:29 +0800

    update roadtax insrance

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v22';
+const CACHE = 'gp-hub-v23';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
