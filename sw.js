a8930bfee539729026c2af7ef100d7f5fc46452e
jimmykuek <jimmykuek94@gmail.com>
2026-06-17 09:29:34 +0800

    gp deposit

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v21';
+const CACHE = 'gp-hub-v22';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
