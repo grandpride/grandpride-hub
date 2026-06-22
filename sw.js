975d0d922c7e6182a4f9ea91c4d417e5110cd3bd
jimmykuek <jimmykuek94@gmail.com>
2026-06-22 10:42:13 +0800

    live map

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v30';
+const CACHE = 'gp-hub-v31';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
