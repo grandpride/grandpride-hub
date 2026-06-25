37452cbe35dc952f699bd4a1da30174e0cbfc6c9
jimmykuek <jimmykuek94@gmail.com>
2026-06-25 16:45:26 +0800

    reset password

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v40';
+const CACHE = 'gp-hub-v41';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
