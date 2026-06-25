40b2bf7d5fc8b1a01fe9a3edf214453ee4667d94
jimmykuek <jimmykuek94@gmail.com>
2026-06-25 16:55:16 +0800

    geo facing

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v41';
+const CACHE = 'gp-hub-v42';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
