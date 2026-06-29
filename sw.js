d4ff0822708ce77a85cb55989b766b7ac58fc70b
jimmykuek <jimmykuek94@gmail.com>
2026-06-29 12:46:59 +0800

    update chauffeur clock in

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v43';
+const CACHE = 'gp-hub-v45';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
