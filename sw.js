820a3019029033bfd0716e87272eff29b2e3a487
jimmykuek <jimmykuek94@gmail.com>
2026-08-21 17:10:28 +0800

    update

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    the CACHE version below and redeploy.
    ============================================================ */
 
-const CACHE = 'gp-hub-v69';
+const CACHE = 'gp-hub-v70';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
